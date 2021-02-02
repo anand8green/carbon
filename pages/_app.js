2077285c8fc802818f5f068045dacecb90dd94e6
Anand Kumar <anandkumar@Anands-iMac.local>
2021-02-02 10:26:05 +0000

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import '../styles/Home.css'
 import CarbonScore from './component/CarbonScore'
 import StateData from './component/StateData'
