3f94bdff08292cd7180ea7c84b564e190beb7180
Anand Kumar <anandkumar@Anands-iMac.local>
2021-02-02 11:29:41 +0000

    update

--- pages/component/CarbonScore.js
+++ pages/component/CarbonScore.js
@@ -1,12 +1,14 @@
 import { globalState } from "./StateData"
-
 export default function CarbonScore() {
 
     const [{ score },] = globalState()
 
     return (
-        <div>
-            <h6>Carbon Score:{score}</h6>
+        <div className="container">
+            <div className="score">
+                <img src="/footprint.png" alt="" />
+                <h6>{score} tonnes</h6>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Index() {
   return (
-    <div>
+    <div className="container">
       <h1>Let's crunch some numbers</h1>
       <p>Before we can offset your carbon footprint, we need to work out how big it is. We know things aren't normal right now, so tell us what you'd normally get up to. It'll take about 3 minutes.</p>
 
