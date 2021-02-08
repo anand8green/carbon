474371639511e6da3ecde556263ee482c40a9238
Anand Kumar <anandkumar@Anands-iMac.local>
2021-02-08 16:17:25 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Index() {
         transition={{ duration: 0.5, type: 'spring', stiffness: 100 }}
       >
         <div className="start__info">
-          <h1>Let's crunch some numbers</h1>
+          <h1>Test : Let's crunch some numbers</h1>
           <p>Before we can offset your carbon footprint, we need to work out how big it is. We know things aren't normal right now, so tell us what you'd normally get up to. It'll take about 3 minutes.</p>
           <Link href="/transport">
             <button>Start</button>
